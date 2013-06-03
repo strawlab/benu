dc79c69c05cc01bf23c31270726845d0d8dfa865
John Stowers <john.stowers@gmail.com>
2013-06-03 18:55:16 +0200

    add nested example

---
+++ examples/nested.py
@@ -0,0 +1,29 @@
+import tempfile
+import random
+import matplotlib.pyplot as plt
+
+from benu.benu import Canvas
+
+OUTER = 500
+
+#---------
+
+cm = plt.get_cmap('jet')
+outer = OUTER
+
+tmp_fname = tempfile.mktemp('.png')
+canv = Canvas(tmp_fname, OUTER, OUTER)
+
+while outer > 10:
+    device_rect = (0,0,outer,outer)
+    user_rect   = (0,0,outer,outer)
+    with canv.set_user_coords(device_rect, user_rect):
+        canv.poly([0,0,outer,outer,0],
+                  [0,outer,outer,0,0],
+                  color_rgba=cm(random.randrange(0,cm.N))
+    )
+    outer = outer // 2
+
+canv.save()
+print tmp_fname
+
