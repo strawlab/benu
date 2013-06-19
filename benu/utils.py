d02de86835d0d9ce104ad4e9ee3471e66dc0524f
John Stowers <john.stowers@gmail.com>
2013-06-19 18:40:24 +0200

    add another geometry negotiation option

--- benu/utils.py
+++ benu/utils.py
@@ -65,17 +65,104 @@ def scale(w, h, x, y, maximum=True):
         return nw or 1, y
     return x, nh or 1
 
+def sorted_left_to_right(panels):
+    return [k for k,v in sorted(panels.items(), key=lambda x: x[1]['device_x0'])]
+
 def negotiate_panel_size(panels, max_height, margin):
     #calculate sizes of the panels that fit
     for name in panels:
         m = panels[name]
-        m['dw'], m['dh'] = scale(
-                            m['width'], m['height'],
-                            m['device_x1']-m['device_x0'], max_height
+        nw, nh = scale(
+                    m['width'], m['height'],
+                    m['device_x1']-m['device_x0'], max_height
         )
+        m['dw'], m['dh'] = nw, nh
 
     #recalculate the actual sized output
     actual_out_h = np.max([panels[name]['dh'] for name in panels]) + 2*margin
     actual_out_w = np.sum([panels[name]['dw'] for name in panels]) + 2*margin
 
     return actual_out_w, actual_out_h
+
+def negotiate_panel_size_same_height(panels, max_width, margin):
+    #increase the size of all individual boxes until the total width does not
+    #exceed max_width
+    ch = 0
+    while (not ch) or (tw < max_width):
+        widths = []
+        for name in panels:
+            m = panels[name]
+            widths.append( scale(m['width'], m['height'],1e9,ch)[0] )
+        ch += 1
+        tw = np.sum(widths)
+
+    #calculate sizes of the panels that fit
+    for name in panels:
+        m = panels[name]
+        nw, nh = scale(
+                    m['width'], m['height'],
+                    1e9, ch-1
+        )
+        m['dw'], m['dh'] = nw, nh
+
+    #adjust starting x
+    x = None
+    for name in sorted_left_to_right(panels):
+        m = panels[name]
+        if x is None:
+            x = m['device_x0']
+            x += m['dw']
+            continue
+
+        m['device_x0'] = x
+        x += m['dw']
+
+    #recalculate the actual sized output
+    actual_out_h = np.max([panels[name]['dh'] for name in panels])
+    actual_out_w = np.sum([panels[name]['dw'] for name in panels])
+
+    return actual_out_w, actual_out_h
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    TARGET_OUT_W, TARGET_OUT_H = 1024, 768
+    MARGIN = 0
+
+    target_out_w = TARGET_OUT_W
+    target_out_h = TARGET_OUT_H
+    device_y0 = MARGIN
+    device_y1 = target_out_h-MARGIN
+    max_height = device_y1-device_y0
+    max_width = TARGET_OUT_W
+
+    for fmf,dsc in [((659,494),(1024,768)),((1920,1080),(1024,768))]:
+        fmfwidth,fmfheight = fmf
+        dscwidth,dscheight = dsc
+
+        panels = {}
+        panels["mov"] = dict(
+            width = fmfwidth,
+            height = fmfheight,
+            device_x0 = MARGIN,
+            device_x1 = target_out_w//2 - MARGIN//2,
+        )
+        panels["dsc"] = dict(
+            width = dscwidth,
+            height = dscheight,
+            device_x0 = target_out_w//2 + MARGIN//2,
+            device_x1 = target_out_w - MARGIN//2,
+        )
+
+        hpanels = panels.copy()
+        wpanels = panels.copy()
+
+        actual_out_w, actual_out_h = negotiate_panel_size_same_height(panels, max_width, MARGIN)
+        print "SAME HEIGHT MOVIE SIZE", actual_out_w, actual_out_h
+        pprint(hpanels)
+
+        actual_out_w, actual_out_h = negotiate_panel_size(wpanels, max_height, MARGIN)
+        print "MAX WIDTH MOVIE SIZE", actual_out_w, actual_out_h
+        pprint(wpanels)
+
+
