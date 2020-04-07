21ee755196b04eb5104c41183922f438216289f7
Andrew Straw <strawman@astraw.com>
2020-04-07 17:55:48 +0200

    load long_description from README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
+from os import path
+from io import open
+
+# read the contents of README.md
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
-    name="benu", description="python plotting tool", packages=["benu"], version="0.1.0",
+    name="benu",
+    description="python plotting tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["benu"],
+    version="0.1.0",
 )
